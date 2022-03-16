271c9c50d1ccf26af4f48a677e28df57aec1dad9
lhwza007 <lhwza007x2@gmail.com>
2022-03-16 09:30:48 +0700

    Create Exercise5_1_Ratchanon_A.py
    homework5_1

---
+++ Exercise5_1_Ratchanon_A.py
@@ -0,0 +1,12 @@
+a = int(input("number :"))
+b = int(input("number :"))
+
+ans1 = a + b
+ans2 = a - b
+ans3 = a * b
+ans4 = a / b
+
+print(a, "+", b, "=",ans1)
+print(a, "-", b, "=",ans2)
+print(a, "*", b, "=",ans3)
+print(a, "/", b, "=",ans4)
