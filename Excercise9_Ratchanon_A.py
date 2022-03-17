954a82724fb6905c9336d6fcbf32e7405e25c617
lhwza007 <lhwza007x2@gmail.com>
2022-03-17 18:11:06 +0700

    Create Excercise9_Ratchanon_A.py
    homework

---
+++ Excercise9_Ratchanon_A.py
@@ -0,0 +1,9 @@
+userid = input("Id :")
+userpass = input("Pass :")
+
+while userid != "lhwza007" or userpass != "12345":
+    print("Pleas try again")
+    userid = input("Id :")
+    userpass = input("Pass :")
+
+print("Login Success")
