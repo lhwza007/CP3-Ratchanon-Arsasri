6e968bca87abeb82d3787da8ab51490c0959847e
lhwza007 <lhwza007x2@gmail.com>
2022-03-19 04:40:14 +0700

    Create Lecture50_Ratchanon_A.py

---
+++ Lecture50_Ratchanon_A.py
@@ -0,0 +1,16 @@
+def addNumber1(x,y,z):
+    print(x + y + z)
+
+def addNumber2(x,y,z):
+    print(x - y - z)
+
+def addNumber3(x,y,z):
+    print(x * y * z)
+
+def addNumber4(x,y,z):
+    print(x / y / z)
+
+addNumber1(10,20,5)
+addNumber2(20,10,5)
+addNumber3(2,4,2)
+addNumber4(50,10,2)
